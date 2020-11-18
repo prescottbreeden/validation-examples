aeee7a37768b27864791a700c493ea8810c6b951
prescottbreeden <prescott.breeden@gmail.com>
2020-11-17 16:32:21 -0800

    added prettier and husky with new scripts

--- src/validations/contact.test.ts
+++ src/validations/contact.test.ts
@@ -1,8 +1,28 @@
-import { ContactValidation } from "./contact.validation";
+import { ContactValidation } from './contact.validation';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { emptyContact } from 'types/Contact.type';
+import { emptyPhone } from 'types/Phone.type';
 
-describe("contact validations", () => {
-  it("validate All", () => {
-    const v = ContactValidation();
-    expect(true).toBe(true);
+describe('contact validations', () => {
+  describe('allValid', () => {
+    it('validates a valid payload', () => {
+      const { result } = renderHook(() => ContactValidation());
+      const contact = {
+        ...emptyContact(),
+        name: 'bob ross',
+        phones: [
+          {
+            ...emptyPhone(),
+            number: '(123) 123 - 1234',
+            description: 'cell',
+          },
+        ],
+      };
+      act(() => {
+        result.current.validateAll(contact);
+      });
+      expect(result.current.validationErrors).toEqual([]);
+      expect(result.current.isValid).toBe(true);
+    });
   });
 });
