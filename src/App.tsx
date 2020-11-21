6c0ade4a3ed58cf30519346e69371f34fdb5dd8f
prescottbreeden <prescott.breeden@gmail.com>
2020-11-21 00:34:11 -0800

    styling and form names

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,35 @@ function App() {
   return (
     <div className="App">
       <h1>Validation Demo</h1>
+      <p>
+        Tab through the fields to trigger validations on blur and edit to see
+        errors get removed.
+      </p>
       <Box className="display-box">
+        <p className="u-align-right">phone.form.tsx</p>
         <CreatePhone />
       </Box>
       <FlexRow>
         <FlexColumn>
           <Box className="display-box">
+            <p className="u-align-right">cat.form.tsx</p>
             <CreateCat />
           </Box>
         </FlexColumn>
         <FlexColumn>
           <Box className="display-box">
+            <p className="u-align-right">dog.form.tsx</p>
             <CreateDog />
           </Box>
         </FlexColumn>
       </FlexRow>
       <Box className="display-box">
+        <div className="u-align-right">
+          <p>contact.form.tsx</p>
+          <p>phone.form.tsx</p>
+          <p>cat.form.tsx</p>
+          <p>dog.form.tsx</p>
+        </div>
         <CreateContact />
       </Box>
     </div>
